b34c1dc318093c5b404796cd5749ca5a89e6b346
MaximShishkin <yacyfra@gmail.com>
2021-01-22 19:32:48 +0300

    first commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,40 @@
+
+public class MyFirstClass {
+    // Точка входа
+	public static void main(String[] args) {
+		// Если ты это прочитал, то пиши погнали в чат!
+		int a; // int = integer = целое число (-1, 10, 15, 0)
+        a=-10; // Инициализация
+       // System.out.print(a+" пробел ");   
+        a=10;
+        
+        int b;
+        b=158;
+       // System.out.print(b); 
+        
+        double c; // дробные или вещественные ( 3.14)
+        c = 3.14; // инициализация
+        System.out.println("Значение переменной с = "+ c);  
+        
+       // System.out.print(a+b+c);
+        // System.out.println("Переводит на новую строку");
+        
+        boolean bol = true; // Правда и ложь
+        // правда (да) = true
+        System.out.println("bol");
+        System.out.println(bol);
+        
+        boolean bol1 = false; 
+        // нет (ложь) = false
+        System.out.println(bol1);
+        
+        // строки
+        String str = "Java";
+        System.out.println(str);
+        String str1 = "остров";
+        System.out.println(str1);
+        System.out.println(str+" не только лишь "+str1); // склеивание строк
+	}
+
+}
+
